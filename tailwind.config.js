72e36c51da9936210a2a42c3acfd92e1a85752a7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 14:56:43 +0100

    chore: animation + color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,33 @@ export default {
       fontFamily: {
         nunito: ["Nunito", "sans-serif"],
       },
+      colors: {
+        warning: "#C61531",
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "slide-up": "slide-up 0.5s ease-out both",
+        "fade-in": "fade-in 0.6s ease-out both",
+      },
     },
   },
   darkMode: "class",
