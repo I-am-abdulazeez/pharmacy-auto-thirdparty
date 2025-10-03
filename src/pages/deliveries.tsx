9cf0f23135a04cb67631b592f9d3a2c587fa0ea5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 20:19:59 +0100

    feat: Deliveries page

---
+++ src/pages/deliveries.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/layouts/default";
+
+export default function DeliveryiesPage() {
+  return (
+    <DefaultLayout>
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Create Delivery</h2>
+        <p>This is where you can create a new delivery.</p>
+      </div>
+    </DefaultLayout>
+  );
+}
