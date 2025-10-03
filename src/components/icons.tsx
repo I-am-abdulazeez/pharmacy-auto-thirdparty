7d33893f1a22de5ced1731b56f73f76a0e55a4db
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 14:58:14 +0100

    feat: ArrowRightIcon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -15,6 +15,25 @@ export const DeleteIcon = (props: IconSvgProps) => {
   );
 };
 
+export const ArrowRightIcon = (props: IconSvgProps) => {
+  return (
+    <svg
+      className="h-5 w-5 flex-shrink-0 text-[#F15A24] transition-transform duration-300 group-hover:translate-x-1"
+      fill="none"
+      {...props}
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path
+        d="M13 7l5 5m0 0l-5 5m5-5H6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+      />
+    </svg>
+  );
+};
+
 export const EditIcon = (props: IconSvgProps) => {
   return (
     <svg
