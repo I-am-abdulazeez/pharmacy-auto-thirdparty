a00b2f8ee52eabbcd86457ef98302ae2786f1d13
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 20:20:10 +0100

    feat: Enrollee page

---
+++ src/pages/enrollees.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/layouts/default";
+
+export default function EnrolleesPage() {
+  return (
+    <DefaultLayout>
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Enrollees Page</h2>
+        <p>This is where you can manage enrollees.</p>
+      </div>
+    </DefaultLayout>
+  );
+}
