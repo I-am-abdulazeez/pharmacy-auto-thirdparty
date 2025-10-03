6dc49b03720a6d62bb515e807b194fe988d651e5
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 20:20:25 +0100

    feat: pharmacy page

---
+++ src/pages/pharmacy.tsx
@@ -0,0 +1,15 @@
+import DefaultLayout from "@/layouts/default";
+
+export default function PharmacyPage() {
+  return (
+    <DefaultLayout>
+      <div>
+        <h2 className="text-2xl font-semibold mb-4">Pharmacy Dashboard</h2>
+        <p>
+          Welcome to the Pharmacy Dashboard. Here you can manage your
+          operations.
+        </p>
+      </div>
+    </DefaultLayout>
+  );
+}
