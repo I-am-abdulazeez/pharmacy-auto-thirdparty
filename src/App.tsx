45739900865c46193b4dac87f9dff06648229447
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 14:56:56 +0100

    feat: Provider login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import IndexPage from "@/pages/index";
 import LeadwayLoginPage from "@/pages/leadway-login";
+import ProviderLoginPage from "@/pages/provider-login";
 
 function App() {
   return (
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<LeadwayLoginPage />} path="/leadway-login" />
-      {/* <Route element={<ProviderLoginPage />} path="/provider-login" /> */}
+      <Route element={<ProviderLoginPage />} path="/provider-login" />
     </Routes>
   );
 }
