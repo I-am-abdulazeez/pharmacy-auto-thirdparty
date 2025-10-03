2a13d471fba18edfd76e66c0c10395e76f0011f1
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 20:18:24 +0100

    chore: new pages (Deliveries, Phramacy, and Enrollees)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "@/pages/index";
 import LeadwayLoginPage from "@/pages/leadway-login";
 import ProviderLoginPage from "@/pages/provider-login";
+import PharmacyPage from "@/pages/pharmacy";
+import EnrolleesPage from "@/pages/enrollees";
+import DeliveriesPage from "@/pages/deliveries";
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Route element={<IndexPage />} path="/" />
       <Route element={<LeadwayLoginPage />} path="/leadway-login" />
       <Route element={<ProviderLoginPage />} path="/provider-login" />
+
+      <Route element={<PharmacyPage />} path="/pharmacy" />
+      <Route element={<DeliveriesPage />} path="/deliveries" />
+      <Route element={<EnrolleesPage />} path="/enrollees" />
     </Routes>
   );
 }
