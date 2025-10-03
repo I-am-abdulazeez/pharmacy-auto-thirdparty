b86dbf210624237e3aa38a7d2e2a663f82eca954
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 14:57:30 +0100

    feat: GridPattern component

---
+++ src/components/grid-pattern.tsx
@@ -0,0 +1,7 @@
+export default function GridPattern() {
+  return (
+    <div className="absolute inset-0 opacity-[0.03]">
+      <div className="absolute inset-0 bg-[repeating-linear-gradient(0deg,#1A1A1A,#1A1A1A_1px,transparent_1px,transparent_40px),repeating-linear-gradient(90deg,#1A1A1A,#1A1A1A_1px,transparent_1px,transparent_40px)]" />
+    </div>
+  );
+}
