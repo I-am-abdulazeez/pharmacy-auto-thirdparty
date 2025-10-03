cc997a5acfb63fac4b2d54672f4a2613a7f6b94f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 13:58:09 +0100

    feat: text-showcase component

---
+++ src/components/text-showcase.tsx
@@ -0,0 +1,25 @@
+export default function TextShowcase({
+  showDescription = true,
+}: {
+  showDescription?: boolean;
+}) {
+  return (
+    <div className="mb-7">
+      <img
+        alt="Logo"
+        className="mx-auto h-16 mb-7 w-auto"
+        src="/leadway-logo.png"
+      />
+
+      <h1 className="text-[25px] text-center font-bold text-[#1A1A1A]">
+        Where genuine care meets{" "}
+        <span className="text-[#f15A24]">unparalleled service.</span>
+      </h1>
+      {showDescription && (
+        <p className="mt-6 text-md font-medium text-center text-gray-600">
+          Log in to request a prescription refill for an Enrollee
+        </p>
+      )}
+    </div>
+  );
+}
