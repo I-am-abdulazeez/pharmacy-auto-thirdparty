d672dc6e51ea151e993159cadf8470f6cdee653d
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-03 14:59:02 +0100

    feat: button slide effect

---
+++ src/components/effects/slide.tsx
@@ -0,0 +1,5 @@
+export default function SlideEffect() {
+  return (
+    <span className="absolute inset-0 w-0 bg-[#F15A24] opacity-10 transition-all duration-300 group-hover/back:w-full" />
+  );
+}
