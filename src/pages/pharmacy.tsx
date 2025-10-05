25cf962daa0ddc278f8590d1a257aae1ef47f42a
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-05 17:33:22 +0100

    chore: pharmacy

--- src/pages/pharmacy.tsx
+++ src/pages/pharmacy.tsx
@@ -1,15 +1,37 @@
+import { Input } from "@heroui/input";
+
+import SelectStates from "@/components/select-state";
+import PharmacyDataTable from "@/components/pharmacy-table";
+import { useEnrolleeSearch } from "@/lib/hooks/use-enrollee-search";
 import DefaultLayout from "@/layouts/default";
+import PageHeader from "@/components/ui/page-header";
 
 export default function PharmacyPage() {
+  const { searchCriteria, handleChange, handleBlur, validation } =
+    useEnrolleeSearch();
+
   return (
     <DefaultLayout>
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Pharmacy Dashboard</h2>
-        <p>
-          Welcome to the Pharmacy Dashboard. Here you can manage your
-          operations.
-        </p>
+      <PageHeader
+        description="Manage and view pharmacy information for enrollees"
+        title="Pharmacy"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-5">
+        <Input
+          errorMessage={validation.enrolleeId.errorMessage}
+          isDisabled={false}
+          isInvalid={validation.enrolleeId.isInvalid}
+          label="Enrollee ID"
+          name="enrolleeId"
+          placeholder="Enter Enrollee ID (e.g. 2400135/0)"
+          radius="sm"
+          value={searchCriteria.enrolleeId}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+        <SelectStates />
       </div>
+      <PharmacyDataTable />
     </DefaultLayout>
   );
 }
