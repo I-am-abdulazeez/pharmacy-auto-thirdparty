cabb1c9f561b03e2790b16f9734e882d0de4b1f6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-05 17:54:21 +0100

    chore: Enrollee page

--- src/pages/enrollees.tsx
+++ src/pages/enrollees.tsx
@@ -1,12 +1,13 @@
+import PageHeader from "@/components/ui/page-header";
 import DefaultLayout from "@/layouts/default";
 
 export default function EnrolleesPage() {
   return (
     <DefaultLayout>
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Enrollees Page</h2>
-        <p>This is where you can manage enrollees.</p>
-      </div>
+      <PageHeader
+        description="Manage and view enrollee information"
+        title="Enrollees"
+      />
     </DefaultLayout>
   );
 }
