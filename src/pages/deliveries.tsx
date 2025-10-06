24f6da54d93ef10f66b8e49853f68ac1535d18b8
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-06 16:03:36 +0100

    feat: Deliveries page ready

--- src/pages/deliveries.tsx
+++ src/pages/deliveries.tsx
@@ -1,12 +1,13 @@
+import PageHeader from "@/components/ui/page-header";
 import DefaultLayout from "@/layouts/default";
 
 export default function DeliveryiesPage() {
   return (
     <DefaultLayout>
-      <div>
-        <h2 className="text-2xl font-semibold mb-4">Create Delivery</h2>
-        <p>This is where you can create a new delivery.</p>
-      </div>
+      <PageHeader
+        description="Manage and view delivery information"
+        title="Deliveries"
+      />
     </DefaultLayout>
   );
 }
