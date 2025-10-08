00f916d49f6e414eae0594e59ebec074ffe65f11
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-10-08 06:39:58 +0100

    feat: load diagnosis pre-app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
+import { initializeDiagnosesData } from "./lib/services/diagonosis-service.ts";
+import { initializeProvidersData } from "./lib/services/fetch-pro-select.ts";
+import { deliveryFormState } from "./lib/store/delivery-store.ts";
+import { initializeProceduresData } from "./lib/services/procedure-service.ts";
+
+initializeDiagnosesData();
+initializeProvidersData();
+
+const pharmacyid = deliveryFormState.get().pharmacyId;
+
+if (pharmacyid !== 0) {
+  initializeProceduresData();
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
         <App />
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            duration: 5000,
+          }}
+        />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
