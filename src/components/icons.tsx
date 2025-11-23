20eec0a8937361bb94e794a80cdbc1b534f31539
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-11-23 15:31:49 +0100

    chore: add restore icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -45,6 +45,24 @@ export const PendingIcon = (props: IconSvgProps) => (
   </svg>
 );
 
+export const RestoreIcon = ({ size = 16 }) => (
+  <svg
+    fill="none"
+    height={size}
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    width={size}
+  >
+    <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+    <path d="M21 3v5h-5" />
+    <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
+    <path d="M3 21v-5h5" />
+  </svg>
+);
+
 export const ArrowRightIcon = (props: IconSvgProps) => {
   return (
     <svg
